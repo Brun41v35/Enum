8fa95fcf80e7619ff96f557631e0759cca74dd2e
Bruno Alves <alves.bruno945@gmail.com>
2020-06-10 11:23:41 -0300

    Enum
    Using Enum in Swift

---
+++ Enum.playground/Contents.swift
@@ -0,0 +1,12 @@
+// This document, i will talk about "Enum". Enum is a type represents a group named options
+// Example below
+enum Directions {
+    case right
+    case left
+}
+
+var direction = Directions.right
+print(direction)
+
+
+// This is a simple example
